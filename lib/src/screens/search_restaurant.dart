5a21f6a59a03d036e4277a9231fc98f8658698d2
Muhammad Dadang Setiawan <fzpromds@gmail.com>
2023-12-11 22:20:50 +0700

    done

--- lib/src/screens/search_restaurant.dart
+++ lib/src/screens/search_restaurant.dart
@@ -25,84 +25,96 @@ class _SearchRestaurantScreenState extends State<SearchRestaurantScreen> {
     return ChangeNotifierProvider<SearchRestaurantProv>(
       create: (context) => SearchRestaurantProv(),
       builder: (context, child) {
-        return Padding(
-            padding: const EdgeInsets.all(15),
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                        color: Colors.grey.shade100,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(20))),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: TextField(
-                        controller: _textController,
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Search restaurant or menu name',
-                            suffixIcon: GestureDetector(
-                                onTap: () {
-                                  Provider.of<SearchRestaurantProv>(context,
-                                          listen: false)
-                                      .searchRestaurant(_textController.text);
-                                },
-                                child: const Icon(Icons.search)),
-                            suffixIconColor: Colors.deepPurple),
-                      ),
+        return Scaffold(
+          appBar: AppBar(
+            toolbarHeight: 80,
+            flexibleSpace: Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      color: Colors.grey.shade100,
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(20))),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: TextField(
+                      controller: _textController,
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Search restaurant or menu name',
+                          suffixIcon: GestureDetector(
+                              onTap: () {
+                                Provider.of<SearchRestaurantProv>(context,
+                                        listen: false)
+                                    .searchRestaurant(_textController.text);
+                              },
+                              child: const Icon(
+                                Icons.search,
+                                size: 35,
+                              )),
+                          suffixIconColor: Colors.deepPurple),
                     ),
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  context.watch<SearchRestaurantProv>().isLoading == true
-                      ? const Center(
-                          child: CircularProgressIndicator(),
-                        )
-                      : context.watch<SearchRestaurantProv>().result.founded > 0
-                          ? SizedBox(
-                              width: MediaQuery.of(context).size.width,
-                              height: MediaQuery.of(context).size.height * 0.8,
-                              child: ListView.builder(
-                                  itemCount: context
-                                      .watch<SearchRestaurantProv>()
-                                      .result
-                                      .founded,
-                                  itemBuilder: (context, index) {
-                                    var restaurant = context
+                ),
+              ),
+            ),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    context.watch<SearchRestaurantProv>().isLoading == true
+                        ? const Center(
+                            child: CircularProgressIndicator(),
+                          )
+                        : context.watch<SearchRestaurantProv>().result.founded >
+                                0
+                            ? SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                height:
+                                    MediaQuery.of(context).size.height * 0.8,
+                                child: ListView.builder(
+                                    itemCount: context
                                         .watch<SearchRestaurantProv>()
                                         .result
-                                        .restaurants[index];
-                                    return GestureDetector(
-                                        onTap: () {
-                                          context.pushNamed('detail',
-                                              pathParameters: {
-                                                "id": restaurant.id,
-                                              });
-                                        },
-                                        child: RestaurantSearchedCard(
-                                            restaurant: restaurant));
-                                  }),
-                            )
-                          : context
-                                      .watch<SearchRestaurantProv>()
-                                      .result
-                                      .founded ==
-                                  0
-                              ? Center(
-                                  child: Text(context
-                                      .watch<SearchRestaurantProv>()
-                                      .message),
-                                )
-                              : const SizedBox()
-                ],
+                                        .founded,
+                                    itemBuilder: (context, index) {
+                                      var restaurant = context
+                                          .watch<SearchRestaurantProv>()
+                                          .result
+                                          .restaurants[index];
+                                      return GestureDetector(
+                                          onTap: () {
+                                            context.pushNamed('detail',
+                                                pathParameters: {
+                                                  "id": restaurant.id,
+                                                });
+                                          },
+                                          child: RestaurantSearchedCard(
+                                              restaurant: restaurant));
+                                    }),
+                              )
+                            : context
+                                        .watch<SearchRestaurantProv>()
+                                        .result
+                                        .founded ==
+                                    0
+                                ? Center(
+                                    child: Text(context
+                                        .watch<SearchRestaurantProv>()
+                                        .message),
+                                  )
+                                : const SizedBox()
+                  ],
+                ),
               ),
-            ));
+            ),
+          ),
+        );
       },
     );
   }
